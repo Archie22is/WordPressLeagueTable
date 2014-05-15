6e4e43d4dc9b0a8ca58533e0e64986f1820c65fe
Archie Makuwa <archie@aatsol.co.za>
2014-05-15 15:52:26 +0200

    name changes

--- foosball-rankings.php
+++ foosball-rankings.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Plugin Name: Creative Spark Foosball Rankings
- * Plugin URI: http://www.creativespark.co.za/plugins/
+ * Plugin Name: Foosball Rankings
+ * Plugin URI: http://www.aatsol.co.za/plugins/
  * Description: That plugin that helps other brag about their foosball skills on the internet...
  * Author: Archie Makuwa
  * Version: 1.00
@@ -14,6 +14,3 @@ include plugin_dir_path( __FILE__ ) . '/inc/class-foosball-rankings-admin.php';
 
 // Shortcode and Template Tag
 #include plugin_dir_path( __FILE__ ) . '/inc/class-foosball.php';
-
-// Widget
-#include plugin_dir_path( __FILE__ ) . '/inc/class-bs-testimonials-widget.php';
